58cc37185982e67a194d8e8118bae8bfa6393254
Naji-97 <elbairabennaji@gmail.com>
2023-01-18 14:44:34 +0100

    Create Navbar and footer and make my project like spa

--- script.js
+++ script.js
@@ -77,3 +77,38 @@ window.onload = () => {
   }
   savebook.BookData.forEach((item) => DisplayBooks(item));
 };
+
+// a complete website with navigation
+const list = document.getElementById('list');
+const addNew = document.getElementById('add-new');
+const contact = document.getElementById('contact');
+const listBook = document.querySelector('.list-container');
+const addNewSect = document.querySelector('.new-book-container');
+const contactSect = document.getElementById('contact-sect');
+
+list.addEventListener('click', () => {
+  addNewSect.classList.add('non-display');
+  listBook.classList.remove('non-display');
+  contactSect.classList.add('non-display');
+  list.classList.add('active');
+  addNew.classList.remove('active');
+  contact.classList.remove('active');
+});
+
+addNew.addEventListener('click', () => {
+  addNewSect.classList.remove('non-display');
+  listBook.classList.add('non-display');
+  contactSect.classList.add('non-display');
+  list.classList.remove('active');
+  addNew.classList.add('active');
+  contact.classList.remove('active');
+});
+
+contact.addEventListener('click', () => {
+  addNewSect.classList.add('non-display');
+  listBook.classList.add('non-display');
+  contactSect.classList.remove('non-display');
+  list.classList.remove('active');
+  contact.classList.add('active');
+  addNew.classList.remove('active');
+});
